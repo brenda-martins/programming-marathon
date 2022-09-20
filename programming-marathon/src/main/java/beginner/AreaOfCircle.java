d613260770bbbf12a20df2969aeb8f5f77d90482
Brenda Martins <brenda.martins-ext@ab-inbev.com>
2022-09-20 19:44:49 -0300

    Resolved begginer exercises

---
+++ programming-marathon/src/main/java/beginner/AreaOfCircle.java
@@ -0,0 +1,19 @@
+package beginner;
+
+import java.util.Scanner;
+
+
+public class AreaOfCircle {
+
+    private static final Double n = 3.14159;
+
+    public static void main(String[] args){
+        Scanner scanner =  new Scanner(System.in);
+
+        var R = scanner.nextDouble();
+        var A = n * Math.pow(R, 2);
+
+        System.out.println(String.format("A=%.4f", A));
+
+    }
+}
---
+++ programming-marathon/src/main/java/beginner/ExtremalyBasic.java
@@ -0,0 +1,18 @@
+package beginner;
+
+
+import java.util.Scanner;
+
+public class ExtremalyBasic {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        var A = scanner.nextInt();
+        var B = scanner.nextInt();
+
+        var X = A + B;
+
+        System.out.println("X = " + X);
+
+    }
+}
