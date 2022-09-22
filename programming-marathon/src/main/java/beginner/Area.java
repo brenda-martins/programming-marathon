4c2ed90f6ca9d16198e71b5487b3370bbd874a71
Brenda Martins <brenda.martins-ext@ab-inbev.com>
2022-09-21 21:02:51 -0300

    Created Area program

---
+++ programming-marathon/src/main/java/beginner/Area.java
@@ -0,0 +1,30 @@
+package beginner;
+
+import java.util.Scanner;
+
+public class Area {
+
+    private static final  double pi = 3.14159;
+
+
+    public static void main(String[] args){
+        Scanner scanner =  new Scanner(System.in);
+        double A = scanner.nextDouble();
+        double B = scanner.nextDouble();
+        double C = scanner.nextDouble();
+
+        double TRIANGULO = (A*C)/2;
+        double CIRCULO = pi * Math.pow(C, 2);
+        double TRAPEZIO = ((A+B)/2) * C;
+        double QUADRADO = Math.pow(B, 2);
+        double RETANGULO = A * B;
+
+        System.out.println(String.format("TRIANGULO: %.3f", TRIANGULO));
+        System.out.println(String.format("CIRCULO: %.3f", CIRCULO));
+        System.out.println(String.format("TRAPEZIO: %.3f", TRAPEZIO));
+        System.out.println(String.format("QUADRADO: %.3f", QUADRADO));
+        System.out.println(String.format("RETANGULO: %.3f", RETANGULO));
+
+
+    }
+}
